413e464b9e2cd9f3568a602607b7b48023605b00
Massimo12390 <malerbamassimo52@gmail.com>
2025-04-14 09:14:51 +0200

    Add files via upload

---
+++ malerba (1).py
@@ -0,0 +1,57 @@
+// Malerba Massimo 4G
+print("scegli un panino")
+print("digita 1 per bigmc,2 per crispy mcBacon,3 per mc chiken)
+Scelta=int(input(1))
+if scelta==1)://hai scelto il bigmc
+print("Quanti bigmc desideri:")
+sqa bigmc
+Spesa=Spesa+7.00
+elif scelta==2://hai scelto il crispy mcBacon
+print("Quanti crispy mcBacon desideri:")
+sqa crispy mcBacon
+Spesa=Spesa+5.80
+elif scelta==3://hai scelto il mcChiken 
+print("Quanti mcChiken desideri:")
+sqa mcChiken
+Spesa=Spesa+4.50
+print("scegli un secondo")
+print("digita 1 per patatine,digita 2 per mc nuggets,digita 3 per alette di pollo)
+Scelta=int(input(2))
+if scelta==4://hai scelto le patatine
+print("Quante patatine desideri:)
+sqa patatine
+Spesa=Spesa+3.00
+elif Scelta==5://hai scelto mc nuggets
+print("Quanti mc nuggets desideri:")
+sqa mc nuggets
+Spesa=Spesa+4.80
+elif Scelta==6://hai scelto le alette di pollo 
+print("Quante alette di pollo desideri:")
+sqa alette di pollo
+Spesa=Spesa+3.60
+print("Scegli il dessert")
+print("digita 7 per il milkshake,digita 8 per il cornetto al cioccolato,digita 9 per il mc flurry)
+Scelta=int(input(7))
+if Scelta==7://hai scelto milkshake
+print("quanti milkshake desideri:")
+sqa milkshake
+Spesa=Spesa+3.50
+elif Scelta==8://hai scelto il cornetto al cioccolato
+print("Quanti cornetti desideri:")
+sqa cornetti
+Spesa=Spesa+2.70
+elif Scelta==9://hai scelto mcflurry
+print("quanti mcflurry desider:")
+sqa mcflurry
+Spesa=Spesa+3.80
+Totale= {
+          "Panino1"=1,
+		  "Panino2"=2,
+		  "secondo"1=4,
+		  "secondo2"=5,
+		  "dessert1"=7,
+		  "dessert2"=8
+}
+totale=0
+print("Grazie per aver scelto mcDonald il totale è:"Totale)
+
